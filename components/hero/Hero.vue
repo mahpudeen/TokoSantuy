926f4664dcfc30f731c87fc8989e0ab71faa21de
tomieranda <tomierfanda007@gmail.com>
2019-08-07 15:44:10 +0700

    edit frond end

--- components/hero/Hero.vue
+++ components/hero/Hero.vue
@@ -2,12 +2,6 @@
   <div class="hero is-medium is-bold">
     <div class="hero-body">
       <div class="container">
-        <h1 class="title">
-          Free ecommerce template for Vue.js projects
-        </h1>
-        <h2 class="subtitle">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        </h2>
         
       </div>
     </div>
@@ -16,7 +10,8 @@
 
 <style lang="scss" scoped>
     .hero {
-      background: rgb(81, 186, 252);
+      background-image: url('../../static/bgtokosantuy.jpg');
+      height: 510px;
     }
     h1, h2 {
       color: #fff;

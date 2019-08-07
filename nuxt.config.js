b3f4920e2780928a83791013c262862403b62ca6
Dindin Mahpudin <mahpudeen@gmail.com>
2019-08-07 15:50:41 +0700

    Merge

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,7 @@ module.exports = {
       {
         rel: 'stylesheet',
         href:
-          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+          'https://bootswatch.com/4/cosmo/bootstrap.min.css'
       }
     ]
   },
